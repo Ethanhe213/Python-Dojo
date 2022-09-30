8dae66c9460fdda1eeab6e5ffc69ca276950bf73
Ethan He <ethanhe.213@gmail.com>
2022-09-29 23:14:08 -0400

    second commit

---
+++ fundamentals/oop/modularizing/child.py
@@ -0,0 +1,6 @@
+from locale import locale_encoding_alias
+import parent
+if __name__ == "__main__":
+    print("the file is being executed directly")
+else:
+    print("The file is being executed because it is imported by another file. The file is called: ", __name__)
